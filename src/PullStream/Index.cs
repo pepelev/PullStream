ae9a44bf401bf1be55843d33bd5c0c40267167c2
Pepelev <kran440@gmail.com>
2021-04-22 15:19:59 +0500

    Uses Span and Memory from System.Memory package

---
+++ src/PullStream/Index.cs
@@ -0,0 +1,25 @@
+﻿#if NETSTANDARD2_0
+
+// ReSharper disable once CheckNamespace
+namespace System
+{
+    internal readonly struct Index
+    {
+        public int Value { get; }
+        public bool FromEnd { get; }
+
+        public Index(int value, bool fromEnd = false)
+        {
+            Value = value;
+            FromEnd = fromEnd;
+        }
+
+        public static implicit operator Index(int index) => new(index);
+
+        public int GetOffset(int length) => FromEnd
+            ? length - Value
+            : Value;
+    }
+}
+
+#endif
---
+++ src/PullStream/Range.cs
@@ -0,0 +1,19 @@
+﻿#if NETSTANDARD2_0
+
+// ReSharper disable once CheckNamespace
+namespace System
+{
+    internal readonly struct Range
+    {
+        public Index Start { get; }
+        public Index End { get; }
+
+        public Range(Index start, Index end)
+        {
+            Start = start;
+            End = end;
+        }
+    }
+}
+
+#endif
