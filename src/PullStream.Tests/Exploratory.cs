b3e0b35703f8a0df9a3091b19bdb5178ffb16225
Pepelev <kran440@gmail.com>
2021-04-22 15:47:29 +0500

    Adds exporatory test

---
+++ src/PullStream.Tests/Exploratory.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace PullStream.Tests
+{
+    public sealed class Exploratory
+    {
+        [Test]
+        public void Memory_Stream_Allows_To_Read_Zero_Bytes()
+        {
+            var stream = new MemoryStream();
+            stream.Write(new byte[] {1, 2, 3}, 0, 3);
+            stream.Position = 0;
+            var read = stream.Read(new byte[3], 0, 0);
+            read.Should().Be(0);
+        }
+    }
+}
