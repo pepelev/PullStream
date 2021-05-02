ef862362298be91163d57084488084b8169c14ac
Pepelev <kran440@gmail.com>
2021-05-02 18:39:47 +0500

    Introduces OutputChunk and extension methods for it. Fixes #27

---
+++ src/PullStream/SequenceStreamBuilderExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+
+namespace PullStream
+{
+    public static class SequenceStreamBuilderExtensions
+    {
+        public static Stream Build<Chunk, Context>(this SequenceStream.Builder<Chunk, Context> builder)
+            where Chunk : OutputChunk<Context> =>
+            builder.Writing(Write);
+
+        public static Stream Build<Chunk, Context>(this SequenceStream.AsyncBuilder<Chunk, Context> builder)
+            where Chunk : OutputChunk<Context> =>
+            builder.Writing(Write);
+
+        private static void Write<Context>(Context output, OutputChunk<Context> chunk)
+        {
+            chunk.Write(output);
+        }
+    }
+}
